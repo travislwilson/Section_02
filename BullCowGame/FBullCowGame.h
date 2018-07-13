d73ecf7657254c093981b70587e4c3d7d1f84587
travislwilson <travis.wilson.it@gmail.com>
2018-07-12 23:44:52 -0700

    Added FBullCowGame cpp file

--- BullCowGame/FBullCowGame.h
+++ BullCowGame/FBullCowGame.h
@@ -5,7 +5,7 @@ class FBullCowGame {
 public:
 	void Reset(); //TODO make a more rich return value.
 	int GetMaxTries();
-	int GetCurrentTry;
+	int GetCurrentTry();
 	bool IsGameWon();
 	bool CheckGuessValidity(std::string);//TODO make a more rich return value.
 
